bbaf585af666e83ad89487ed61cae6e8957e435b
asultan00 <asultan00@stuy.edu>
2018-10-23 20:03:27 -0400

    finished Phase 1

--- Driver.java
+++ Driver.java
@@ -1,5 +1,20 @@
 public class Driver {
      public static void main(String[] args) {
-
+          System.out.println();
+          //SA0 = ((test)) empty list, size 10
+          SuperArray SA0 = new SuperArray();
+          System.out.println(SA0.toString()); //[]
+          System.out.println(SA0.toStringDebug()); //[null x10]
+          System.out.println();
+          //SA1 = list, size 10
+          SuperArray SA1 = new SuperArray();
+          SA1.add("zero");
+          SA1.add("one");
+          System.out.println(SA1.toString()); //[zero, one]
+          System.out.println(SA1.toStringDebug()); //[zero, one, null x8]
+          System.out.println(SA1.get(1)); //one
+          System.out.println(SA1.set(1, "fortnite")); //one
+          System.out.println(SA1.get(1)); //fortnite
+          System.out.println();
      }
 }
--- SuperArray.java
+++ SuperArray.java
@@ -1,3 +1,66 @@
 public class SuperArray {
-
+     private String[] data;
+     private int size;
+     public SuperArray() {
+          size = 0;
+          data = new String[10];
+     }
+     public void clear() {
+          size = 0;
+     }
+     public int size() {
+          return size;
+     }
+     public boolean isEmpty() {
+          return ( size == 0 );
+     }
+     public boolean add(String e) {
+          if ( size < data.length ) {
+               data[size] = e;
+               size += 1;
+               return true;
+          }
+          else {
+               return false;
+          }
+     }
+     public String toString() {
+          String result = "[";
+          if (data[0] != null) {
+               for (int i = 0; i < size - 1; i++) {
+                    result = result + data[i] + ", ";
+               }
+                    result = result + data[size - 1] + "]";
+          }
+          else {
+               result = result + "]";
+          }
+          return result + " // SIZE:" + size;
+     }
+     public String toStringDebug() {
+          String result = "[";
+          for (int i = 0; i < data.length - 1; i++) {
+               result = result + data[i] + ", ";
+          }
+          result = result + data[data.length - 1] + "]";
+          return result + " // SIZE:" + size;
+     }
+     public String get(int i) {
+          if (i < 0 || i >= size()) {
+               return null;
+          }
+          else {
+               return "Value @ index" + i + ": " + data[i];
+          }
+     }
+     public String set(int i, String e) {
+          if (i < 0 || i >= size()) {
+               return null;
+          }
+          else {
+               String r = data[i];
+               data[i] = e;
+               return "Replaced: " + r + " // with: " + e;
+          }
+     }
 }
