b81e39e5e6488a28fd3dddbc8884974887e750fb
asultan00 <asultan00@stuy.edu>
2018-10-25 09:56:00 -0400

    edited Driver

--- Driver.java
+++ Driver.java
@@ -16,6 +16,8 @@ public class Driver {
           System.out.println(SA1.set(1, "fortnite")); //one
           System.out.println(SA1.get(1)); //fortnite
           System.out.println(SA1.toString());
+          SA1.clear();
+          System.out.println(SA1.toStringDebug());
           System.out.println();
           //SA2 = list, size 10 but overflow
           SuperArray SA2 = new SuperArray();
@@ -26,9 +28,6 @@ public class Driver {
           System.out.println(SA2.toStringDebug());//[0, 1, 2... 9]
           SA2.add("no");
           System.out.println(SA2.toString());//[0, 1, 2... 9]
-          for ( int i = 0; i < 10; i++ ) {
-               SA2.add(i + 10 + "");
-          }
           System.out.println(SA2.toStringDebug());
      }
 }
